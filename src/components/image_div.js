f5afc91653daf43403cc7066f313af50adc2863b
UtibeAbasi <1.BarukhOr@gmail.com>
2016-03-26 23:18:22 -0400

    Changes to Image-Div

--- src/components/image_div.js
+++ src/components/image_div.js
@@ -3,8 +3,8 @@ import React,{Component} from 'react'
 export default class ImageDiv  extends Component{
 	render(){
 		return(
-			<div className="imagediv">
-				<img src="build/css/images/img_2.jpg"/>
+			<div className="image-div">
+				
 			</div>
 		)
 	}
