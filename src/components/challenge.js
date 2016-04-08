b62f2449be539ed2f33f1c801ccbac343367e61e
UtibeAbasi <1.BarukhOr@gmail.com>
2016-04-08 19:46:30 -0400

    Added Additional Components
    + New Components + Styling

--- src/components/challenge.js
+++ src/components/challenge.js
@@ -1,17 +1,15 @@
 import React,{Component} from 'react'
-import ApplyButton from './apply_button'
 
-export default class About extends Component{
+export default class Challenge extends Component{
 	render(){
 		return(
-			<div className="about-div container-fluid padder">
+			<div className="challenge-div container-fluid padder">
 				<div className="">
-					<h2 className="about-div-header">// The Challenge</h2>
+					<h2 className="challenge-header">// The Challenge</h2>
 					<div className="container about-div-list">
 						<h3>How do we get more women involved in the Greater New Haven Tech Scene?</h3>
 					</div>
 				</div>
-				<ApplyButton/>
 			</div>
 		)
 	}
--- src/components/schedule.js
+++ src/components/schedule.js
@@ -1,25 +1,24 @@
 import React,{Component} from 'react'
 import ApplyButton from './apply_button'
 
-export default class About extends Component{
+export default class Schedule extends Component{
 	render(){
 		return(
-			<div className="about-div container-fluid padder">
+			<div className="schedule-div container-fluid padder">
 				<div className="">
-					<h2 className="about-div-header">// THE SCHEDULE</h2>
-					<div className="container about-div-list">
+					<h2 className="schedule-header">// THE SCHEDULE</h2>
+					<div className="container schedule-content">
 						<h3>Friday</h3>
 						<p>3:15 YouthZone Session One: Learn basic HTML by remixing websites about current events</p>
 						<p>4:15 YouthZone Session Two: Evaluate the 13 principles of a feminist Web</p>
 						<p>5:15 YouthZone Session Three: Create a movie poster using HTML and CSS</p>
 						<h3>Saturday</h3>
-						<p>8:30-9:00   Registration
+						<p>8:30-9:00   Registration</p>
 						<p>9:00-9:45   Icebreaker: If you have never built a website learn how as we send letters to the next president</p>
 						<p>12:30       Lunch  </p>    
 						<p>4:30-5:00   Judging</p>     
 					</div>
 				</div>
-				<ApplyButton/>
 			</div>
 		)
 	}
