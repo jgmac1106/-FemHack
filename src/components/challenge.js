86ebcb4ca6ef7d6e84202dbe5b8c901b4f2f4b72
Greg McVerry <jgregmcverry@gmail.com>
2016-04-08 21:14:12 -0400

    Update challenge.js

--- src/components/challenge.js
+++ src/components/challenge.js
@@ -6,8 +6,8 @@ export default class Challenge extends Component{
 			<div className="challenge-div container-fluid padder">
 				<div className="">
 					<h2 className="challenge-header">// The Challenge</h2>
-					div className="femhack-img-div">
-					<img className="femhack-header-img" src={require('../css/images/binarylogotransparent.png')}/>
+					div className="challenge-img-div">
+					<img className="challenge-header-img" src={require('../css/images/binarylogotransparent.png')}/>
 				</div>
 					<div className="container about-div-list">
 						<h3>How do we get more women involved in the Greater New Haven Tech Scene?</h3>
